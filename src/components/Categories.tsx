0ddfcc4e35f90eaf71a3d66accbff72596c5f292
Zhaslanuly Sabyrzhan <thatonesabyrka@gmail.com>
2026-04-28 23:17:45 +0500

    feat: make Categories component dynamic with state

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,13 +1,30 @@
+import { useState } from 'react'
+import clsx from 'clsx'
+
 export const Categories = () => {
+  const [activeCategory, setActiveCategory] = useState(1)
+
+  const categories = [
+    { id: 1, label: 'Все' },
+    { id: 2, label: 'Мясные' },
+    { id: 3, label: 'Вегетарианская' },
+    { id: 4, label: 'Гриль' },
+    { id: 5, label: 'Острые' },
+    { id: 6, label: 'Закрытые' },
+  ]
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map(({ id, label }) => (
+          <li
+            key={id}
+            className={clsx(id === activeCategory && 'active')}
+            onClick={() => setActiveCategory(id)}
+          >
+            {label}
+          </li>
+        ))}
       </ul>
     </div>
   )
