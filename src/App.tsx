774885d687eed49bb5bc82ea86e613267f1d1431
Zhaslanuly Sabyrzhan <thatonesabyrka@gmail.com>
2026-04-28 21:13:55 +0500

    feat: initial components and styles setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { Header } from './components/Header'
+import { Categories } from './components/Categories'
+import { Sort } from './components/Sort'
+import { PizzaBlock } from './components/PizzaBlock'
+
 export const App = () => {
-  return <h1>Hello World</h1>
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {[...Array(8)].map((_, i) => (
+              <PizzaBlock key={i} title="Мексиканская пицца" price={350} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
