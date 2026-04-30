6450cc43709d9ab8627c2df12d83602704e232b2
Zhaslanuly Sabyrzhan <thatonesabyrka@gmail.com>
2026-04-30 06:10:11 +0500

    feat: setup routing using react-router v7 and nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,13 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
 import { Header } from './components/Header'
-import { Categories } from './components/Categories'
-import { Sort } from './components/Sort'
-import { PizzaBlock, Skeleton } from './components/PizzaBlock'
-
-interface Pizza {
-  id: number
-  imageUrl: string
-  title: string
-  types: number[]
-  sizes: number[]
-  price: number
-  category: number
-  rating: number
-}
+import { Outlet } from 'react-router'
 
 export const App = () => {
-  const [pizzas, setPizzas] = useState<Pizza[]>([])
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    const fetchPizzas = async () => {
-      try {
-        const { data } = await axios.get<Pizza[]>(
-          'https://b31c7cb4cec48f6f.mokky.dev/pizzas',
-        )
-        setPizzas(data)
-      } catch (error) {
-        console.error(error)
-      } finally {
-        setIsLoading(false)
-      }
-    }
-
-    fetchPizzas()
-  }, [])
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
-              : pizzas.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
-          </div>
+          <Outlet />
         </div>
       </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './styles/main.scss'
 import { App } from './App'
+import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
+import { Cart } from './pages/Cart'
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root')
+
+createRoot(root!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
